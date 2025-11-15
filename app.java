dc89998269a5b74cdd6161211f65de1d57f0fb40
Anveshreddy96 <anvesh.justdoit@gmail.com>
2025-11-15 13:47:37 +0530

    Update app.java

--- app.java
+++ app.java
@@ -15,16 +15,4 @@ here im writing v2 code
     }
 }
 
-here im writing v3 code
-    v3 code begins here
-    
-    public class Factorial {
-    public static void main(String[] args) {
-        int num = 5;
-        long factorial = 1;
-        for (int i = 1; i <= num; ++i) {
-            factorial *= i;
-        }
-        System.out.println("Factorial of " + num + " is: " + factorial);
-    }
-}
+
